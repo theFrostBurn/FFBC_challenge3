f22eefc0762d4ced178c9aaac0d4e7126f6b0e03
Cookie Monster <51273391+theFrostBurn@users.noreply.github.com>
2024-12-04 08:05:38 +0900

    flag #1

--- lib/presentation/widgets/home/home_app_bar.dart
+++ lib/presentation/widgets/home/home_app_bar.dart
@@ -1,43 +1,45 @@
 import 'package:flutter/cupertino.dart';
 
-class HomeAppBar extends CupertinoNavigationBar {
-  const HomeAppBar({super.key})
-      : super(
-          middle: const Text(
-            'Music',
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          trailing: const _TrailingButtons(),
-        );
-}
-
-class _TrailingButtons extends StatelessWidget {
-  const _TrailingButtons();
+class HomeAppBar extends StatelessWidget
+    implements ObstructingPreferredSizeWidget {
+  const HomeAppBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Row(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        CupertinoButton(
-          padding: EdgeInsets.zero,
-          onPressed: null,
-          child: Icon(CupertinoIcons.bell),
+    return CupertinoNavigationBar(
+      middle: const Text(
+        'Music Player',
+        style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
         ),
-        CupertinoButton(
-          padding: EdgeInsets.zero,
-          onPressed: null,
-          child: Icon(CupertinoIcons.search),
-        ),
-        CupertinoButton(
-          padding: EdgeInsets.zero,
-          onPressed: null,
-          child: Icon(CupertinoIcons.profile_circled),
-        ),
-      ],
+      ),
+      trailing: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          CupertinoButton(
+            padding: EdgeInsets.zero,
+            onPressed: () {},
+            child: const Icon(CupertinoIcons.search),
+          ),
+          CupertinoButton(
+            padding: EdgeInsets.zero,
+            onPressed: () {},
+            child: const Icon(CupertinoIcons.bell),
+          ),
+          CupertinoButton(
+            padding: EdgeInsets.zero,
+            onPressed: () {},
+            child: const Icon(CupertinoIcons.person_circle),
+          ),
+        ],
+      ),
     );
   }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(44.0);
+
+  @override
+  bool shouldFullyObstruct(BuildContext context) => true;
 }
