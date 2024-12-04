fbcea77eaa653304c1c2f7a3354706de25f45ef6
Cookie Monster <51273391+theFrostBurn@users.noreply.github.com>
2024-12-04 15:39:08 +0900

    white mode

--- lib/presentation/widgets/home/recommended_playlists.dart
+++ lib/presentation/widgets/home/recommended_playlists.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/cupertino.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 
 class RecommendedPlaylists extends StatelessWidget {
   const RecommendedPlaylists({super.key});
@@ -7,16 +6,15 @@ class RecommendedPlaylists extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final playlists = [
-      {'title': 'K-POP 히트곡 모음', 'imageUrl': '...'},
-      {'title': '2024 최신 트렌드', 'imageUrl': '...'},
-      {'title': '인기 팝송 플레이리스트', 'imageUrl': '...'},
-      {'title': '드라이브 뮤직', 'imageUrl': '...'},
-      {'title': '편안한 어쿠스틱', 'imageUrl': '...'},
-      {'title': '운동할 때 듣기 좋은', 'imageUrl': '...'},
-      {'title': '공부할 때 집중되는', 'imageUrl': '...'},
-      {'title': '출근길 플레이리스트', 'imageUrl': '...'},
-      {'title': '잔잔한 재즈 컬렉션', 'imageUrl': '...'},
-      {'title': '인디음악 베스트', 'imageUrl': '...'},
+      {'title': 'K-POP 히트곡 모음', 'imageId': '200'},
+      {'title': '드라이브 뮤직', 'imageId': '201'},
+      {'title': '편안한 어쿠스틱', 'imageId': '202'},
+      {'title': '운동할 때 듣기 좋은', 'imageId': '203'},
+      {'title': '공부할 때 집중되는', 'imageId': '204'},
+      {'title': '출근길 플레이리스트', 'imageId': '205'},
+      {'title': '잔잔한 재즈 컬렉션', 'imageId': '206'},
+      {'title': '인디음악 베스트', 'imageId': '207'},
+      {'title': '2024 최신 트렌드', 'imageId': '208'},
     ];
 
     return SliverToBoxAdapter(
@@ -24,77 +22,56 @@ class RecommendedPlaylists extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16),
+            padding: EdgeInsets.fromLTRB(16, 32, 16, 16),
             child: Text(
               '추천 플레이리스트',
               style: TextStyle(
-                fontSize: 22,
+                fontSize: 20,
                 fontWeight: FontWeight.bold,
               ),
             ),
           ),
-          const SizedBox(height: 16),
           SizedBox(
             height: 200,
             child: ListView.builder(
               scrollDirection: Axis.horizontal,
-              padding: const EdgeInsets.symmetric(horizontal: 16),
+              padding: const EdgeInsets.symmetric(horizontal: 12),
               itemCount: playlists.length,
               itemBuilder: (context, index) {
                 final playlist = playlists[index];
                 return Padding(
-                  padding: const EdgeInsets.only(right: 16),
-                  child: CupertinoButton(
-                    padding: EdgeInsets.zero,
-                    onPressed: () {},
-                    child: Container(
-                      width: 160,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(8),
-                        color: CupertinoColors.systemGrey6,
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            height: 160,
-                            decoration: BoxDecoration(
-                              borderRadius: const BorderRadius.vertical(
-                                top: Radius.circular(8),
-                              ),
+                  padding: const EdgeInsets.symmetric(horizontal: 4),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        width: 160,
+                        height: 160,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          color: CupertinoColors.systemGrey5,
+                          image: DecorationImage(
+                            image: NetworkImage(
+                              'https://picsum.photos/seed/${playlist['imageId']}/160',
                             ),
-                            clipBehavior: Clip.antiAlias,
-                            child: CachedNetworkImage(
-                              imageUrl: playlist['imageUrl']!,
-                              fit: BoxFit.cover,
-                              placeholder: (context, url) => const Center(
-                                child: CupertinoActivityIndicator(),
-                              ),
-                              errorWidget: (context, url, error) =>
-                                  const Center(
-                                child: Icon(
-                                  CupertinoIcons.music_note_2,
-                                  size: 48,
-                                  color: CupertinoColors.systemGrey,
-                                ),
-                              ),
-                            ),
+                            fit: BoxFit.cover,
                           ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(
-                              playlist['title']!,
-                              style: const TextStyle(
-                                fontSize: 14,
-                                color: CupertinoColors.black,
-                              ),
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                          ),
-                        ],
+                        ),
                       ),
-                    ),
+                      const SizedBox(height: 8),
+                      SizedBox(
+                        width: 160,
+                        child: Text(
+                          playlist['title']!,
+                          style: const TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500,
+                          ),
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    ],
                   ),
                 );
               },
